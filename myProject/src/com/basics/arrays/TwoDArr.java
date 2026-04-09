169b62345a102706d050df2da7c45d6ea8acecc9
manohar628 <venkatamanohar62@gmail.com>
2026-04-09 17:11:32 +0530

    2D array

--- myProject/src/com/basics/arrays/TwoDArr.java
+++ myProject/src/com/basics/arrays/TwoDArr.java
@@ -9,7 +9,7 @@ public class TwoDArr {
 		
 		
 	for(int i=0;i<matrix.length;i++) {
-		for(int j=0; j<matrix[0].length;j++) {
+		for(int j=0 ; j<matrix[i].length;j++) {
 			System.out.print(matrix[i][j]+ " ");
 		}
 		System.out.println( );
